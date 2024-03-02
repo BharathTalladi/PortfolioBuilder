8edf302266d086855ee75ee6fdd786a58473d0a0
Bharath Talladi <61378031+BharathTalladi@users.noreply.github.com>
2024-03-01 18:27:10 -0600

    Update UserPlanComponent.tsx

--- employee-recurring-plans-frontend/src/Components/UserPlanComponent.tsx
+++ employee-recurring-plans-frontend/src/Components/UserPlanComponent.tsx
@@ -26,14 +26,7 @@ const UserPlanComponent = () => {
                 if (id) {
                     const response = await getUserPlanById(id);
                     console.log(response);
-<<<<<<< HEAD
-                    if(response?.data?.errorMessage){
-                        navigate('/createRecurringPlanByUser');
-                    }
-                    setUserData(response.data);
-=======
                     response?.data?.errorMeesage? navigate('/createRecurringPlanByUser') : setUserData(response.data);
->>>>>>> 7c9bd91ce127746823e69587423b6eef5ac872c5
                 }
             } catch (error) {
                 console.error('Error fetching user plan:', error);
@@ -44,13 +37,7 @@ const UserPlanComponent = () => {
             fetchData();
         }
     }, [id,navigate]);
-
-<<<<<<< HEAD
-    return (
-        <>
-=======
     return ( userData?.age ? (
->>>>>>> 7c9bd91ce127746823e69587423b6eef5ac872c5
                 <TableContainer component={Paper}>
                            <Table>
                                 <TableHead sx={{ marginTop:"10px" }} >
@@ -121,16 +108,9 @@ const UserPlanComponent = () => {
                         <Button sx={{ marginLeft: "10px", backgroundColor: "#FFF" }} variant="contained">
                             <NavLink to="/createUserPlan">Create User Plan</NavLink>
                         </Button>
-<<<<<<< HEAD
-                    )}
-                </TableContainer>
-            
-        </>
-=======
                         </>
                     )
-                
->>>>>>> 7c9bd91ce127746823e69587423b6eef5ac872c5
+            
     );
 };
 
